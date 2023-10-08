4de102af729661da158a79b15d16139aed324abb
vishnuxx <vishnuvs2002it@gmail.com>
2023-10-08 12:30:09 +0530

    Add: Route Navigation in homepage

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css"
+import { APPROUTES } from "../../constants/AppRoutes";
 
 function Home() {
     return (
 			<main className={styles.main}>
 				<h1 className={styles.h1}>TaskMate</h1>
 				<h3 className={styles.h3}>Your ToDo Companion</h3>
-				<button className={styles.startbutton}>GetStarted</button>
+				<Link to={APPROUTES.todo}>
+					<button className={styles.startbutton}>GetStarted</button>
+				</Link>
 				<img className={styles.bgimg} src="https://media.istockphoto.com/id/1334887411/vector/concept-of-checklist.jpg?s=612x612&w=0&k=20&c=kiQYNroY9BgIeTRKp7raM7gPjxFCBqWn47DT8GF1a8g=" alt="ng" />
 			</main>
 		);
