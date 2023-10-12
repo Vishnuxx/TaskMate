8efd030b141407a59c295f155e59195931888557
vishnuxx <vishnuvs2002it@gmail.com>
2023-10-12 18:45:36 +0530

    Setup Routers for home , auth , todo pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import "./styles/App.css";
-import "./styles/globals.css"
-import Home from "./pages/Home/page";
+import "./styles/globals.css";
+
+
+import { APPROUTES } from "./constants/AppRoutes";
+
+import AuthPage from "./pages/Auth/Auth";
+import Login from "./components/Auth/Login/Login";
+import Signup from "./components/Auth/Signup/Signup";
+import Home from "./pages/Home/home";
+import TodoPage from "./pages/Todo/todo";
+
 
 function App() {
 	return (
 		<>
 			<Routes>
-        <Route path="/" element={<Home/>}/>
-      </Routes>
+				<Route path={APPROUTES.home} element={<Home/>} />
+				<Route path={APPROUTES.auth} element={<AuthPage />}>
+					<Route path={APPROUTES.login} element={<Login />} />
+					<Route path={APPROUTES.signup} element={<Signup />} />
+				</Route>
+				<Route path={APPROUTES.todo} element={<TodoPage />} />
+			</Routes>
 		</>
 	);
 }
