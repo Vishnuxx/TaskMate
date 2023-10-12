79d55d8a1809eefdb4795330ed4a4c9600315d39
vishnuxx <vishnuvs2002it@gmail.com>
2023-10-12 18:31:30 +0530

    refactored button styles

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -8,7 +8,7 @@ function Home() {
 				<h1 className={styles.h1}>TaskMate</h1>
 				<h3 className={styles.h3}>Your ToDo Companion</h3>
 				<Link to={APPROUTES.todo}>
-					<button className={styles.startbutton}>GetStarted</button>
+					<button className={"primary-button" }>GetStarted</button>
 				</Link>
 				<img className={styles.bgimg} src="https://media.istockphoto.com/id/1334887411/vector/concept-of-checklist.jpg?s=612x612&w=0&k=20&c=kiQYNroY9BgIeTRKp7raM7gPjxFCBqWn47DT8GF1a8g=" alt="ng" />
 			</main>
